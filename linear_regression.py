73760a11d3e492565aa8000d49801bf213a995ab
Axel Hoizai <ahoizai@c1r1p8.42lehavre.fr>
2026-04-29 12:14:20 +0200

    clean: add comment & display graphs in same window

--- linear_regression.py
+++ linear_regression.py
@@ -16,22 +16,19 @@ def normalize(array):
     return np.asarray(normalized_data)
 
 def linear_regression(learning_rate, nb_iter):
+    # Get data from file
     df = load(sys.argv[1])
     print(df)
     mileage = np.array(df["km"]).astype(float)
-    print("-" * 50)
-    print("mileage:\n", mileage)
-
     price = np.array(df["price"]).astype(float)
-    print("-" * 50)
-    print("price:\n", price)
 
+    # Normalize mileage
     x_normalized = normalize(mileage)
-    # y_normalized = normalize(df["price"].astype(float))
-    print("-" * 50)
-    print("x_normalized:\n", x_normalized)
-    # print("-" * 50)
-    # print("y_normalized:\n", y_normalized)
+
+    #Init graphs
+    fig, axs = plt.subplots(1, 2, figsize=(15, 6))
+    loss = axs[0]
+    linear = axs[1]
 
     costs = []
     alpha = learning_rate / len(mileage)
@@ -39,49 +36,38 @@ def linear_regression(learning_rate, nb_iter):
     for i in range(nb_iter):
         tmp0 = []
         tmp1 = []
+        # Apply gradient descent
         for j in range(len(mileage)):
             tmp0.append(estimatePrice(x_normalized[j], theta0, theta1) - price[j])
             tmp1.append(tmp0[j] * x_normalized[j])
         nptmp0 = np.array(tmp0)
-        # print("-" * 50)
-        # print("nptmp0:\n", nptmp0)
         nptmp1 = np.array(tmp1)
-        # print("-" * 50)
-        # print("nptmp1:\n", nptmp1)
-
         tmp_theta0 = theta0 - alpha * np.sum(nptmp0)
         tmp_theta1 = theta1 - alpha * np.sum(nptmp1)
         theta0 = tmp_theta0
         theta1 = tmp_theta1
-        # plt.plot(i, theta0, 'o', color='b')
-        # plt.plot(i, theta1, 'o', color='r')
-        # costs.append(cost(x_normalized, y_normalized, theta0, theta1))
-        # x_values = np.linspace(-2, 3, 100)
+
+        # Apply loss function
+        costs.append(cost(x_normalized, price, theta0, theta1))
+
+        # Display each adjusted affine curves
         x_values = np.linspace(-2, 3, 100)
-        # print("-" * 50)
-        # print("x_values:\n", x_values)
         y_pred = (theta1 * x_values) + theta0
-        # print("-" * 50)
-        # print("Y_pred:\n", y_pred)
-        plt.plot(x_values, y_pred, color=(0,1,0,0.5), zorder=2)
+        linear.plot(x_values, y_pred, color=(0,1,0,0.5), zorder=2)
         plt.draw()
 
-    print("theta0:", theta0)
-    print("theta1:", theta1)
-    plt.plot(x_values, y_pred, color='red')
+    # Display loss for each iteration
+    loss.set_title("Loss Function")
+    loss.set_ylabel("Loss")
+    loss.set_xlabel("Nb of iterations")
+    loss.plot(np.arange(nb_iter), costs, color='b')
 
-    # print("COST:\n", costs)
-    # x_values = np.linspace(-2, 3, 100)
-    # print("-" * 50)
-    # print("x_values:\n", x_values)
-    # y_pred = (theta1 * x_values) + theta0
-    # print("-" * 50)
-    # print("Y_pred:\n", y_pred)
-    # plt.plot(x_values, y_pred, color='red', zorder=1)
-
-    plt.scatter(x_normalized, price, color='b', zorder=3)
-    plt.xlabel("mileage")
-    plt.ylabel("price")
+    # Display final affine line and data
+    linear.set_title("Linear regression")
+    linear.scatter(x_normalized, price, color='b', zorder=3)
+    linear.plot(x_values, y_pred, color='red')
+    linear.set_xlabel("Normalized mileage")
+    linear.set_ylabel("Price")
     plt.show()
 
 def main():
